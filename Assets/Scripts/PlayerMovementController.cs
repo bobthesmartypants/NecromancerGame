c3d6024160fd535561f3388e9605e89a5c1785db
Daniel Liu <hi.daniel.liu@gmail.com>
2020-03-10 03:30:08 -0400

    added comments and cleaned some code

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,89 +5,109 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    #region Constants
+    private const float HAND_DISTANCE = 1.4f; // Distance of player's hand from player's body
+    private const float HAND_HEIGHT = 1.5f; // Height of player's hand from ground
+    private const float ATTACK_RECHARGE = 1f; // Time it takes for player to recharge its attack
+    private const float PLAYER_SPEED = 15.0f; // Player running speed
+    #endregion
 
-    private const float HAND_DISTANCE = 1.4f;
-    private const float HAND_HEIGHT = 1.5f;
-    private const float ATTACK_RECHARGE = 1f;
+    #region Private Variables
+    // Rigidbody reference
+    private Rigidbody rb;
 
-    private bool canAttack;
-
+    // Animator references
+    private Animator animator;
     private Animator AttackAnim;
 
+    // Boolean for checking whether player can currently attack
+    private bool canAttack;
+
+    // Reference to ResurrectionCircle script
     private ResurrectionCircle resCircle;
 
-    public float radius;
+    // Current horizontal and vertical input
+    private float moveHorizontal;
+    private float moveVertical;
+    // Mouse position relative to player position
+    private Vector3 relMousePos;
 
-    /*
-    public Vector3 velocity
-    {
-        get { return rb.velocity; }
-    }
-    */
+    // Reference to transform of Hand GameObject
+    private Transform hand;
 
-    public Vector3 velocity;
+    #endregion
 
-    float playerSpeed = 15.0f;
-    Magic equipedMagic;
-    public Sword sword;
+    // Note sure if this is still useful soon
+    Magic equippedMagic;
 
-    float moveHorizontal;
-    float moveVertical;
-    Vector3 relMousePos;
+    #region Public Variables
+    public float radius; // Radius of player. TODO: use C# getters and setters instead
 
-    Animator animator;
-    Rigidbody rb;
+    public Vector3 velocity; // Velocity of player. TODO: use C# getters and setters instead
 
-    Transform hand;
+    public Sword sword; // Reference to Sword script
+    #endregion
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize Rigidbody reference
         rb = GetComponent<Rigidbody>();
 
+        // Initialize sprite animator
         GameObject sprite = transform.Find("sprite").gameObject;
         animator = sprite.GetComponent<Animator>();
 
+        // Initialize radius of player
         Vector3 spriteBounds = sprite.GetComponent<SpriteRenderer>().bounds.size;
         radius = spriteBounds.x / 2;
 
-        equipedMagic = gameObject.AddComponent<Fire>();
+        // Initialize magic
+        equippedMagic = gameObject.AddComponent<Fire>();
+
+        // Point player sprite in same direction as camera
         transform.forward = -Camera.main.transform.forward;
         transform.up = Camera.main.transform.up;
 
-        //Attach sword to hand
+        // Initialize sword and place it in hand
         hand = transform.Find("Hand");
         sword.transform.parent = hand;
         sword.transform.localPosition = Vector3.zero;
         sword.transform.localRotation = Quaternion.identity;
 
+        // Initialize attack animation
         AttackAnim = sword.GetComponent<Animator>();
 
+        // Initialize resurrection circle reference
         resCircle = gameObject.GetComponentInChildren<ResurrectionCircle>();
 
+        // Set initial attack state to true
         canAttack = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // If left button clicked and currently can attack
         if (Input.GetMouseButton(0) && canAttack)
         {
-            equipedMagic.Cast();
-            AttackAnim.SetTrigger("Attack");
-            canAttack = false;
-            StartCoroutine("RechargeAttack");
+            equippedMagic.Cast(); // Cast the current magic
+            AttackAnim.SetTrigger("Attack"); // Do attack animation
+            canAttack = false; // Disable attack
+            StartCoroutine("RechargeAttack"); // Reenable attack after recharged
         }
 
-        if (Input.GetMouseButton(1))
+        // If right mouse button clicked
+        if (Input.GetMouseButton(1)) 
         {
+            // Resurrect all enemies within the resurrection circle
             resCircle.ResurrectEnemies(transform);
         }
 
     }
-
+    
+    // Sets canAttack to true after ATTACK_RECHARGE seconds
     IEnumerator RechargeAttack()
     {
         yield return new WaitForSeconds(ATTACK_RECHARGE);
@@ -96,6 +116,7 @@ public class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Get input from movement and get relative mouse position to player position
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
         relMousePos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
@@ -107,23 +128,33 @@ public class PlayerMovementController : MonoBehaviour
         // Set sword rotation
         sword.SetRotation(Vector3.SignedAngle(relMousePos, Vector3.right, Vector3.forward));
 
+        // Calculate movement vector
         velocity = Vector3.zero;
-        Vector3 movement = moveVertical * new Vector3(0.0f, 0.0f, 1.0f) + moveHorizontal * new Vector3(1.0f, 0.0f, 0.0f);
-        if (movement.magnitude > 1.0f)
+        Vector3 movement = moveVertical * Vector3.forward + moveHorizontal * Vector3.right;
+        if (movement.magnitude > 1.0f) // Normalize movement vector
         {
             movement = movement.normalized;
         }
+
+        // Animate player sprite based on inputs
         animator.SetFloat("movement", movement.magnitude);
         animator.SetFloat("facingY", relMousePos.y);
         animator.SetFloat("facingX", relMousePos.x);
 
+        // Actually move player if movement is big enough
         if (movement.magnitude > 0.25f)
         {
             Vector3.Normalize(movement);
-            animator.SetFloat("dy", movement.z);
+
+            // Pass in relevant information for running animation 
             animator.SetFloat("dx", movement.x);
-            velocity = movement.normalized * playerSpeed;
-            transform.Translate(movement.normalized * playerSpeed * Time.fixedDeltaTime, Space.World);
+            animator.SetFloat("dy", movement.z);
+
+            // Set new velocity
+            velocity = movement.normalized * PLAYER_SPEED;
+
+            // Actually move player
+            transform.Translate(movement.normalized * PLAYER_SPEED * Time.fixedDeltaTime, Space.World);
         }
 
     }
